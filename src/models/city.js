dd39643b5898b187f09815391943e8a04a2c5ef6
Rohit Sharma <rohit.sh2630@gmail.com>
2026-06-19 14:54:58 +0530

    Added airports models

--- src/models/city.js
+++ src/models/city.js
@@ -5,6 +5,9 @@ module.exports = (sequelize, DataTypes) => {
   class City extends Model {
     static associate(models) {
       // define association here
+      this.hasMany(models.Airport,{
+        foreignKey:'cityId'
+      });
     }
   }
 
